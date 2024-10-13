9b1466d456894a4006ca5450b68661b4b7a6c69e
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-10-13 15:45:02 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -9,35 +9,53 @@ from models import User
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username']
-        )
+        user = User(username=json['username'])
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            # Updated to use SQLAlchemy 2.0's Session.get() method
+            user = db.session.get(User, user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'error': 'Invalid credentials'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
